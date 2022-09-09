3e079014027417e18efec2fc2cb51d2f97661f20
glebradnikov <glebr4dnikov@gmail.com>
2022-09-09 19:47:05 +0300

    feat: arrange the file sctructure according to the rules bem nested

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@
 $(document).ready(function () {
   $('.header__burger').click(function (event) {
     $('.header__nav').toggleClass('header__nav_active');
-    $('.header__burger').toggleClass('header__burger_active');
     $('.page').toggleClass('page_locked');
   });
 });
@@ -11,7 +10,6 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.header__link').click(function (event) {
     $('.header__nav').removeClass('header__nav_active');
-    $('.header__burger').removeClass('header__burger_active');
     $('.page').removeClass('page_locked');
   });
 });
